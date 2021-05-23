541daf3325a025ee31dd6a43cbae2ece7b9bada0
Kevin Jin <tost8295@gmail.com>
2021-05-24 02:22:58 +0900

    Delete unecessary typeorm config code side config service

--- server/src/shared/config/config.service.ts
+++ server/src/shared/config/config.service.ts
@@ -32,45 +32,4 @@ export class ConfigService {
   getJwtSecret(): string {
     return this.envConfig['JWT_SECRET']
   }
-
-  get typeOrmConfig(): TypeOrmModuleOptions {
-    let entities = [__dirname + '/../../modules/**/*.entity{.ts,.js}']
-    let migrations = [__dirname + '/../../migrations/*{.ts,.js}']
-
-    if ((<any>module).hot) {
-      const entityContext = (<any>require).context(
-        './../../modules',
-        true,
-        /\.entity\.ts$/,
-      )
-      entities = entityContext.keys().map((id) => {
-        const entityModule = entityContext(id)
-        const [entity] = Object.values(entityModule)
-        return entity
-      })
-      const migrationContext = (<any>require).context(
-        './../../migrations',
-        false,
-        /\.ts$/,
-      )
-      migrations = migrationContext.keys().map((id) => {
-        const migrationModule = migrationContext(id)
-        const [migration] = Object.values(migrationModule)
-        return migration
-      })
-    }
-    return {
-      entities,
-      migrations,
-      keepConnectionAlive: true,
-      type: 'postgres',
-      host: this.get('DB_HOST'),
-      port: this.getNumber('DB_PORT'),
-      username: this.get('DB_USERNAME'),
-      password: this.get('DB_PASSWORD'),
-      database: this.get('DB_DATABASE'),
-      migrationsRun: true,
-      logging: this.nodeEnv === 'development',
-    }
-  }
 }
