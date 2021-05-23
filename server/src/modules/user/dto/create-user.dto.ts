75371304c6ba9e2453270e6dc9b375b8c5bd9e56
Kevin Jin <tost8295@gmail.com>
2021-05-24 03:26:30 +0900

    Add CreateUserDto

---
+++ server/src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,59 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import {
+  IsEmail,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  Length,
+  Max,
+  Min,
+} from 'class-validator'
+import { UserRole } from 'src/shared/constants/constants'
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: UserRole.STUDENT,
+  })
+  @IsEnum(UserRole)
+  readonly role: UserRole
+
+  @ApiProperty({
+    example: 'test@test.com',
+  })
+  @IsEmail()
+  readonly email: string
+
+  @ApiProperty({
+    example: '1234qwer',
+  })
+  @IsString()
+  @Length(1, 50)
+  readonly password: string
+
+  @ApiProperty({
+    example: '김명호',
+  })
+  @IsString()
+  readonly fullName: string
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Max(12)
+  readonly grade: number
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Max(50)
+  readonly classNumber: number
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsPhoneNumber('KR')
+  readonly phone: string
+}
