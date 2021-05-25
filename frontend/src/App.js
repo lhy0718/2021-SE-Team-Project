e2c40605749b36bc06cbbecb6c1bdaeda07579d8
jjonyo <jonghyuk96@cau.ac.kr>
2021-05-25 16:27:15 +0900

    add Signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import LayoutCtrl from './components/LayoutCtrl'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
   const [userObj, setUserObj] = useState({
@@ -20,7 +21,14 @@ function App() {
       <BrowserRouter>
         <LayoutCtrl userObj={userObj}>
           {/* <Route path="/" exact component={!!userObj ? Home : Login} /> */}
-          <Login />
+          <Route
+            path="/signup/teacher"
+            render={() => <Signup isStudentScreen={false} />}
+          />
+          <Route
+            path="/signup/student"
+            render={() => <Signup isStudentScreen={true} />}
+          />
           {/* <Route path="/signup" component={Signup} /> */}
         </LayoutCtrl>
       </BrowserRouter>
