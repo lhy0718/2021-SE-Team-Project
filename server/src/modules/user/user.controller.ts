fd55de435516e4fbb0db9a80dc70861f3265c56b
Kevin Jin <tost8295@gmail.com>
2021-05-25 23:47:23 +0900

    WIP: Add user service and controllers

---
+++ server/src/modules/user/user.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { EmailVerificationParams } from './dto/email-verification.params'
+import { UserService } from './user.service'
+
+@Controller('users')
+@ApiTags('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('/email-verification/:email')
+  @HttpCode(HttpStatus.OK)
+  async verifyEmail(@Param() params: EmailVerificationParams): Promise<any> {
+    if (!/@cau.ac.kr\s*$/.test(params.email)) {
+      throw new HttpException('FORBIDDEN_DOMAIN', HttpStatus.FORBIDDEN)
+    }
+
+    const isDuplicated = await this.userService.checkDuplicate(params.email)
+
+    if (isDuplicated) {
+      throw new HttpException('DUPLICATED_EMAIL', HttpStatus.CONFLICT)
+    }
+
+    return {
+      isDuplicated,
+    }
+  }
+}
