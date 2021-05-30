36dba09a871e6c90d757b6488fce3a03351f68c9
jjonyo <jonghyuk96@cau.ac.kr>
2021-05-30 16:25:19 +0900

    수강목록 api

--- frontend/src/components/StudentHome.js
+++ frontend/src/components/StudentHome.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react'
 import LectureTemplate from './LectureTemplate'
 import { sharedTemplate, shareLectures } from './constants'
 
-function StudentHome({ userObj }) {
-  const lectures = shareLectures
+function StudentHome({ userObj, lectures }) {
+  lectures = !!lectures ? lectures : shareLectures
 
   const columns = sharedTemplate
 
