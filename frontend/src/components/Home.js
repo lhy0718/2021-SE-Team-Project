db081029f119d00c17becfcd964b754ffec2669a
jjonyo <jonghyuk96@cau.ac.kr>
2021-05-31 20:34:27 +0900

    나의 수업 조회 교시정보 추가

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,6 +3,21 @@ import axios from 'axios'
 import StudentHome from './StudentHome'
 import TeacherHome from './TeacherHome'
 
+function parseWeekday(day) {
+  let ret = ''
+  if (day.weekday === 'MON') ret += '월'
+  else if (day.weekday === 'TUE') ret += '화'
+  else if (day.weekday === 'WED') ret += '수'
+  else if (day.weekday === 'THU') ret += '목'
+  else if (day.weekday === 'FRI') ret += '금'
+  else if (day.weekday === 'SAT') ret += '토'
+  else if (day.weekday === 'SUN') ret += '일'
+
+  ret += day.period
+
+  return ret
+}
+
 function Home({ userObj }) {
   const [lectures, setLectures] = useState([])
 
@@ -22,8 +37,17 @@ function Home({ userObj }) {
     axios
       .get(url, { params: params, headers: headers })
       .then((res) => {
-        setLectures(res.data)
-        // console.log(res.data)
+        const lectureData = res.data
+        lectureData.map((lec) => {
+          let Times = []
+          lec.lectureTime.map((day) => {
+            Times.push(parseWeekday(day))
+          })
+
+          lec.times = Times.join(',')
+        })
+        console.log(lectureData)
+        setLectures(lectureData)
       })
       .catch((err) => {
         console.log(err)
